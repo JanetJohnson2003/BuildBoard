aa34fb9d452bfb8a6aca0f19b059960efebf21e0
Karthik Raj <karthikraj9000@gmail.com>
2026-06-06 19:08:35 +0530

    Revert "Hardcode tunnel API URL to override Vercel env vars"
    This reverts commit 59e276f1d1ff0b68ace8ff5043917272e95aa26e.

--- buildboard-frontend/src/lib/config.js
+++ buildboard-frontend/src/lib/config.js
@@ -1,4 +1,4 @@
-const DEFAULT_API_URL = 'https://wild-cobras-clean.loca.lt/api';
+const DEFAULT_API_URL = 'https://many-jokes-wash.loca.lt/api';
 
-export const API_URL = DEFAULT_API_URL;
-export const SOCKET_URL = DEFAULT_API_URL.replace(/\/api$/, '');
+export const API_URL = (import.meta.env.VITE_API_URL || DEFAULT_API_URL).replace(/\/$/, '');
+export const SOCKET_URL = API_URL.replace(/\/api$/, '');
